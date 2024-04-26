fdf994fcb052a5ecd6bf3012823452cf18b0a697
itzmeHerby <herambjadhav22@gmail.com>
2024-04-26 17:19:22 +0530

    First commit of bookstore project, project struct

---
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/home")
+	public String home()
+	{
+		return "home" ;
+	}
+	
+	@GetMapping("/products")
+	public String product()
+	{
+		return "products";
+	}
+}
