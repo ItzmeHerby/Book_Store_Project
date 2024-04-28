ff3b2e55732015caf05e2d15d5329d505a578ea0
itzmeHerby <herambjadhav22@gmail.com>
2024-04-28 21:51:11 +0530

    JPA added, methods used to register books in database, showcase books,in view page with the help of thymelead, worked with controllers for saveBook and added a ModelAndView method to display available-books view page git status

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -1,37 +1,53 @@
 package com.bookstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookstore.entity.Book;
+import com.bookstore.repository.BookRepository;
+import com.bookstore.service.BookService;
 
 @Controller
 public class BookController {
 
+	@Autowired
+	private BookService bookService;
+
 	@GetMapping("/home")
-	public String home()
-	{
-		return "home" ;
+	public String home() {
+		return "home";
 	}
-	
+
 	@GetMapping("/book-register")
-	public String bookRegister()
-	{
+	public String bookRegister() {
 		return "book-register";
 	}
-	
+
 	@GetMapping("/available-books")
-	public String availableBooks()
-	{
-		return "available-books" ;
+	public ModelAndView availableBooks() {
+		List<Book> list = bookService.getAllBooks();
+		return new ModelAndView("available-books" , "listOfBooks" , list);
 	}
-	
+
 	@GetMapping("/my-collection")
-	public String myCollection()
-	{
+	public String myCollection() {
 		return "my-collection";
 	}
-	
-	
+
+	@PostMapping("/savebook")
+	public String addBook(@ModelAttribute Book b) // to Binding and exposing the view
+	{
+		bookService.saveBook(b);
+		return "redirect:/available-books"; // After registering a book user is re-directed to available-books page
+	}
+
 }
---
+++ src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,27 @@
+package com.bookstore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.entity.Book;
+import com.bookstore.repository.BookRepository;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private BookRepository bookRepository ;
+	
+	
+	public void saveBook(Book b)
+	{
+		bookRepository.save(b);
+	}
+	
+	public List<Book> getAllBooks()
+	{
+		return bookRepository.findAll();
+	}
+}
